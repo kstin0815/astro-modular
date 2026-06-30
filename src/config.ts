d12598f8b4c156eb5fb8affc7c43012ddbda15be
kstin0815 <33161611+kstin0815@users.noreply.github.com>
2026-06-30 09:44:06 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -185,7 +185,7 @@ export const siteConfig: SiteConfig = {
   // [CONFIG:SITE_URL]
   site: "https://tzulungchang.com",
   // [CONFIG:SITE_TITLE]
-  title: "The Lung Lung Way 漫漫長路",
+  title: "漫漫長路 lunglung0815",
   // [CONFIG:HOMEPAGE_TITLE]
   homepageTitle: "",
   // [CONFIG:SITE_DESCRIPTION]
